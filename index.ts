14a1722d5418f75a0d4691adc503e5f9f6c115a8
Victor Williams <vrwilliams719@gmail.com>
2022-06-28 18:58:49 -0400

    an Update

--- index.ts
+++ index.ts
@@ -7,35 +7,49 @@ const app = express();
 const port = process.env.PORT;
 const API_KEY = process.env.API_KEY ? process.env.API_KEY?.toString(): '';
 
-// const sdk = require('api')('@sensiblehq/v0#5u9923fl27vdvdd');
-
-// sdk.auth(APIKey);
-// sdk['list-configurations']()
-//   .then(res => console.log('This is the SDK Response', res))
-//   .catch(err => console.error(err));
-
-const check = fetch("https://github.com/sensible-hq/sensible-docs/raw/main/readme-sync/assets/v0/pdfs/1_extract_your_first_data.pdf", {
-  method: "POST",
+const exampleOptions = {
+  method: 'GET',
   headers: {
-    Authorization: API_KEY,
-    "Content-Type": "application/pdf"
+    Accept: 'application/json',
+    Authorization: 'Bearer 20bae306043d2978ff0d9762bc828221750774fc6f684e3d5b66c413f267d9ae088e5990cbf364d81e5b22486dcb9d0d9365aa95580247b9f8e5d9adf0ec8fed',
+  },
+};
+
+const example = fetch('https://api.sensible.so/v0/document_types/type-id/configurations', exampleOptions)
+  .then(response => response.json())
+  .then(response => console.log('This is the example response,', response))
+  .catch(err => console.error(err));
+
+const postOptions = {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    Authorization: 'Bearer 20bae306043d2978ff0d9762bc828221750774fc6f684e3d5b66c413f267d9ae088e5990cbf364d81e5b22486dcb9d0d9365aa95580247b9f8e5d9adf0ec8fed',
   },
   body: "@1_extract_your_first_data.pdf"
+};
 
-}).then(res => console.log('This is the POST Request:', res));
+const post = fetch("https://github.com/sensible-hq/sensible-docs/raw/main/readme-sync/assets/v0/pdfs/1_extract_your_first_data.pdf", postOptions)
+.then(res => console.log('This is the POST Request:', res.status))
+.catch(err => console.error(err));
 
-const getCheck = fetch("https://api.sensible.so/v0/extract/title_commitment?environment=development", {
+const getOptions = {
+  method: 'GET',
   headers: {
-    Accept: "application/json",
-    Authorization: API_KEY,
+    Accept: 'application/json',
+    Authorization: 'Bearer 20bae306043d2978ff0d9762bc828221750774fc6f684e3d5b66c413f267d9ae088e5990cbf364d81e5b22486dcb9d0d9365aa95580247b9f8e5d9adf0ec8fed',
   }
-}).then(res => console.log('This is the GET Request:', res));
+};
+
+const get = fetch("https://api.sensible.so/v0/extract/title_commitment?environment=development", getOptions)
+.then(res => console.log('This is the GET Request:', res.status))
+.catch(err => console.error(err));;
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
-  // sdk();
-  check;
-  getCheck;
+  post;
+  get;
+  example;
 });
 
 app.listen(port, () => {
