3f03b64295b27c1e072ab5ad9a67f03e198282f4
Victor Williams <vrwilliams719@gmail.com>
2022-06-29 14:15:04 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const PDF_URL = 'https://github.com/sensible-hq/sensible-docs/raw/main/readme-sy
 const API_KEY = '20bae306043d2978ff0d9762bc828221750774fc6f684e3d5b66c413f267d9ae088e5990cbf364d81e5b22486dcb9d0d9365aa95580247b9f8e5d9adf0ec8fed';
 
 const main = async () => {
-  const doc = await axios(PDF_URL, {
+  const document = await axios(PDF_URL, {
     responseType: 'arraybuffer'
   })
     .then(response => {
@@ -14,12 +14,11 @@ const main = async () => {
   const response = await axios({
     method: "post",
     url: 'https://api.sensible.so/v0/extract/senseml_basics',
-    data: doc,
+    data: document,
     headers: { "Content-Type": `application/pdf`, "Authorization": 'Bearer ' + API_KEY}
   });
 
-  console.log('TEST 123', response.data);
+  console.log('This is our response: \n', 'Status Code:' , response.status, '\n\n', response.data);
 }
-
 
 main();
